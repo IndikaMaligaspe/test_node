918006dab3cd7baff372d538cd7c8c5af4f4f841
indika <indika@osboxes>
2019-06-03 20:42:45 -0400

    react part completed

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,12 +2,12 @@ import React from "react"
 import {render} from "react-dom"
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-
+import FlashCard from './components/FlashCard'
 
 render (
    <div>
    	<Navbar/>
-           <p className="h1">  Hey babe , I am a REACT dude </p>
+          <FlashCard />
         <Footer/>
    </div>
  , document.getElementById("app")	
